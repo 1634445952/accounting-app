1bff9a2edf3cfde1e9ca667a830f05e25ad8caaa
AccountingDeploy <deploy@example.com>
2026-07-31 13:33:06 +0800

    记账工作台 v1 - PWA ready (内联图表库+manifest+图标)

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'accounting-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './apple-touch-icon.png',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then((cache) => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+      .catch(() => {})
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+// 网络优先，保证随时看到最新内容；离线时回退缓存
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .catch(() => caches.match(event.request))
+  );
+});
